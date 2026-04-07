1e46e26537c4d343149e2e041ab99a84b37b2db2
Abdallah-coding <abdallah.benzoubir@gmail.com>
2026-04-07 17:55:49 +0200

    Update exo1.py
    exo 1 ma version

--- exo1.py
+++ exo1.py
@@ -1,28 +1,26 @@
 class MT:
-    def __init__(self, etats, alphabet, alphabet_de_travail, transitions, etat_initial, etat_final, nb_rubans=1):
-        self.etats = etats 
+    def __init__(self, nom, etats, alphabet, alphabet_de_travail,
+                 transitions, etat_initial, etats_finaux, nb_rubans=1):
+        self.nom = nom
+        self.etats = etats
         self.alphabet = alphabet
-        self.alphabet_de_travail = alphabet_de_travail 
+        self.alphabet_de_travail = alphabet_de_travail
         self.transitions = transitions
         self.etat_initial = etat_initial
-        self.etat_final = etat_final
+        self.etats_finaux = etats_finaux
         self.nb_rubans = nb_rubans
 
-mt = MT(etats={'q0', 'q1'}, 
-        alphabet={'0','1'}, 
-        alphabet_de_travail={'0','1','#','|','_'}, 
-        transitions = {('q0', '0'): ('q1', '1', 'R'),('q1', '1'): ('q0', '0', 'L')},
-        etat_initial = 'q0',
-        etat_final = 'q1')
+    def est_final(self, etat):
+        return etat in self.etats_finaux
+
 
 class Configuration:
-    def __init__(self, etat, ruban, tete):
+    def __init__(self, etat, rubans, tetes):
         self.etat = etat
-        self.ruban = ruban
-        self.tete = tete
+        self.rubans = rubans          # liste de rubans, chaque ruban = liste de symboles
+        self.tetes = tetes            # liste des positions de tête
 
-config = Configuration(
-    etat='q0',
-    ruban=['1', '0', '1', '_'],
-    tete=1
-)
+    def copier(self):
+        nouveaux_rubans = [ruban[:] for ruban in self.rubans]
+        nouvelles_tetes = self.tetes[:]
+        return Configuration(self.etat, nouveaux_rubans, nouvelles_tetes)
