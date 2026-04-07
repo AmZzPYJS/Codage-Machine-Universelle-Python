d02d8a73d415af6988af0234cbe64666967b671d
Abdallah-coding <abdallah.benzoubir@gmail.com>
2026-04-07 17:57:33 +0200

    Update exo2.py
    exo 2 correction

--- exo2.py
+++ exo2.py
@@ -1,64 +1,143 @@
 from exo1 import MT, Configuration
 
+
+def nettoyer_ligne(ligne):
+    if "//" in ligne:
+        ligne = ligne.split("//")[0]
+    return ligne.strip()
+
+
+def decouper_csv(ligne):
+    return [x.strip() for x in ligne.split(",") if x.strip() != ""]
+
+
 def charger_mt(nom_fichier):
+    nom = ""
     etats = set()
     alphabet_entree = set()
     alphabet_travail = set()
     transitions = {}
     etat_initial = ""
-    etat_final = ""
+    etats_finaux = set()
+    nb_rubans = None
 
     with open(nom_fichier, "r", encoding="utf-8") as f:
-        for ligne in f:
-            ligne = ligne.strip()
+        lignes = [nettoyer_ligne(ligne) for ligne in f]
 
-            if not ligne:
-                continue
+    lignes = [ligne for ligne in lignes if ligne != ""]
 
-            if ligne.startswith("etats:"):
-                etats = set(ligne.split(":", 1)[1].split(","))
+    i = 0
+    while i < len(lignes):
+        ligne = lignes[i]
 
-            elif ligne.startswith("alphabet_entree:"):
-                alphabet_entree = set(ligne.split(":", 1)[1].split(","))
+        if ligne.startswith("name:"):
+            nom = ligne.split(":", 1)[1].strip()
+            i += 1
+            continue
 
-            elif ligne.startswith("alphabet_travail:"):
-                alphabet_travail = set(ligne.split(":", 1)[1].split(","))
+        if ligne.startswith("init:"):
+            etat_initial = ligne.split(":", 1)[1].strip()
+            etats.add(etat_initial)
+            i += 1
+            continue
 
-            elif ligne.startswith("etat_initial:"):
-                etat_initial = ligne.split(":", 1)[1]
+        if ligne.startswith("accept:"):
+            partie = ligne.split(":", 1)[1].strip()
+            finals = [x.strip() for x in partie.split(",") if x.strip() != ""]
+            for q in finals:
+                etats_finaux.add(q)
+                etats.add(q)
+            i += 1
+            continue
 
-            elif ligne.startswith("etat_final:"):
-                etat_final = ligne.split(":", 1)[1]
+        # transitions sur 2 lignes
+        if "," in ligne:
+            if i + 1 >= len(lignes):
+                raise ValueError("Transition incomplète dans le fichier.")
 
-            elif ligne.startswith("transition:"):
-                contenu = ligne.split(":", 1)[1].strip()
-                gauche, droite = contenu.split("->")
+            ligne_lecture = decouper_csv(lignes[i])
+            ligne_ecriture = decouper_csv(lignes[i + 1])
 
-                etat_courant, symbole_lu = gauche.split(",")
-                nouvel_etat, symbole_ecrit, mouvement = droite.split(",")
+            etat_courant = ligne_lecture[0]
+            symboles_lus = tuple(ligne_lecture[1:])
 
-                transitions[(etat_courant.strip(), symbole_lu.strip())] = (
-                    nouvel_etat.strip(),
-                    symbole_ecrit.strip(),
-                    mouvement.strip()
-                )
+            if nb_rubans is None:
+                nb_rubans = len(symboles_lus)
+
+            if len(symboles_lus) != nb_rubans:
+                raise ValueError("Nombre de rubans incohérent dans une transition.")
+
+            if len(ligne_ecriture) != 1 + 2 * nb_rubans:
+                raise ValueError("Ligne d'écriture invalide : mauvais nombre d'arguments.")
+
+            nouvel_etat = ligne_ecriture[0]
+            symboles_ecrits = tuple(ligne_ecriture[1:1 + nb_rubans])
+            mouvements = tuple(ligne_ecriture[1 + nb_rubans:1 + 2 * nb_rubans])
+
+            transitions[(etat_courant, symboles_lus)] = (
+                nouvel_etat,
+                symboles_ecrits,
+                mouvements
+            )
+
+            etats.add(etat_courant)
+            etats.add(nouvel_etat)
+
+            for s in symboles_lus:
+                alphabet_travail.add(s)
+            for s in symboles_ecrits:
+                alphabet_travail.add(s)
+
+            i += 2
+            continue
+
+        i += 1
+
+    if nb_rubans is None:
+        nb_rubans = 1
+
+    # alphabet d'entrée : on enlève le blanc
+    alphabet_entree = set(alphabet_travail)
+    if "_" in alphabet_entree:
+        alphabet_entree.remove("_")
 
     return MT(
+        nom=nom,
         etats=etats,
         alphabet=alphabet_entree,
         alphabet_de_travail=alphabet_travail,
         transitions=transitions,
-        etat_initial=etat_initial.strip(),
-        etat_final=etat_final.strip()
+        etat_initial=etat_initial,
+        etats_finaux=etats_finaux,
+        nb_rubans=nb_rubans
     )
 
 
 def creer_configuration_initiale(mt, mot_entree):
-    ruban = list(mot_entree) + ["_"]
-    return Configuration(mt.etat_initial, ruban, 0)
+    rubans = []
+    tetes = []
+
+    for i in range(mt.nb_rubans):
+        if i == 0:
+            ruban = list(mot_entree)
+            if len(ruban) == 0:
+                ruban = ["_"]
+            else:
+                ruban.append("_")
+        else:
+            ruban = ["_"]
+
+        rubans.append(ruban)
+        tetes.append(0)
+
+    return Configuration(mt.etat_initial, rubans, tetes)
 
 
 if __name__ == "__main__":
-    mt = charger_mt("machine.txt")
-    config = creer_configuration_initiale(mt, "101")
-    print("Suite de configuration : (", config.tete,",",config.ruban,",",config.etat, ")")
+    mt = charger_mt("machines/q6_compare.tm")
+    config = creer_configuration_initiale(mt, "10#11")
+    print("Machine :", mt.nom)
+    print("Nombre de rubans :", mt.nb_rubans)
+    print("État initial :", config.etat)
+    print("Rubans initiaux :", config.rubans)
+    print("Têtes :", config.tetes)
