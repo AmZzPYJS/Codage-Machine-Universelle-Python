79794ec0ee981ef9595958e16cdfe2215bbdfa12
Abdallah-coding <abdallah.benzoubir@gmail.com>
2026-04-07 17:59:02 +0200

    Update exo3.py
    exo 3 correction

--- exo3.py
+++ exo3.py
@@ -2,54 +2,90 @@ from exo1 import Configuration
 from exo2 import charger_mt, creer_configuration_initiale
 
 
-def un_pas_de_calcul(mt, config):
-    etat_courant = config.etat
-    position_tete = config.tete
-    ruban = config.ruban
+def lire_symbole(config, numero_ruban):
+    position = config.tetes[numero_ruban]
+    ruban = config.rubans[numero_ruban]
 
-    if position_tete >= len(ruban):
+    if position < 0:
+        return "_"
+    if position >= len(ruban):
+        return "_"
+    return ruban[position]
+
+
+def ecrire_symbole(config, numero_ruban, symbole):
+    position = config.tetes[numero_ruban]
+    ruban = config.rubans[numero_ruban]
+
+    if position < 0:
+        while position < 0:
+            ruban.insert(0, "_")
+            config.tetes[numero_ruban] += 1
+            position += 1
+
+    while position >= len(ruban):
         ruban.append("_")
 
-    if position_tete < 0:
-        ruban.insert(0, "_")
-        position_tete = 0
+    ruban[position] = symbole
 
-    symbole_lu = ruban[position_tete]
 
-    if (etat_courant, symbole_lu) not in mt.transitions:
+def deplacer_tete(config, numero_ruban, mouvement):
+    if mouvement == ">":
+        config.tetes[numero_ruban] += 1
+    elif mouvement == "<":
+        config.tetes[numero_ruban] -= 1
+    elif mouvement == "-":
+        pass
+    else:
+        raise ValueError("Mouvement inconnu : " + mouvement)
+
+    # si on dépasse à gauche, on rajoute un blanc
+    if config.tetes[numero_ruban] < 0:
+        config.rubans[numero_ruban].insert(0, "_")
+        config.tetes[numero_ruban] = 0
+
+    # si on dépasse à droite, on rajoute un blanc
+    if config.tetes[numero_ruban] >= len(config.rubans[numero_ruban]):
+        config.rubans[numero_ruban].append("_")
+
+
+def un_pas_de_calcul(mt, config):
+    symboles_lus = []
+
+    for i in range(mt.nb_rubans):
+        symboles_lus.append(lire_symbole(config, i))
+
+    symboles_lus = tuple(symboles_lus)
+    cle = (config.etat, symboles_lus)
+
+    if cle not in mt.transitions:
         return None
 
-    nouvel_etat, symbole_ecrit, mouvement = mt.transitions[(etat_courant, symbole_lu)]
+    nouvel_etat, symboles_ecrits, mouvements = mt.transitions[cle]
 
-    ruban[position_tete] = symbole_ecrit
+    nouvelle_config = config.copier()
+    nouvelle_config.etat = nouvel_etat
 
-    if mouvement == "R":
-        position_tete += 1
-    elif mouvement == "L":
-        position_tete -= 1
+    for i in range(mt.nb_rubans):
+        ecrire_symbole(nouvelle_config, i, symboles_ecrits[i])
 
-    if position_tete >= len(ruban):
-        ruban.append("_")
+    for i in range(mt.nb_rubans):
+        deplacer_tete(nouvelle_config, i, mouvements[i])
 
-    if position_tete < 0:
-        ruban.insert(0, "_")
-        position_tete = 0
-
-    return Configuration(
-        etat=nouvel_etat,
-        ruban=ruban,
-        tete=position_tete
-    )
+    return nouvelle_config
 
 
 if __name__ == "__main__":
-    mt = charger_mt("machine.txt")
-    config = creer_configuration_initiale(mt, "101")
-    print("Avant : (", config.tete,",", config.ruban,",", config.etat ,")")
+    mt = charger_mt("machines/q6_compare.tm")
+    config = creer_configuration_initiale(mt, "10#11")
+
+    print("Avant :")
+    print(config.etat, config.rubans, config.tetes)
 
     nouvelle_config = un_pas_de_calcul(mt, config)
 
     if nouvelle_config is None:
-        print("Aucune transition = Configuration rejetée")
+        print("Aucune transition possible.")
     else:
-        print("Après : (", nouvelle_config.tete,",", nouvelle_config.ruban,",", nouvelle_config.etat,")")
+        print("Après :")
+        print(nouvelle_config.etat, nouvelle_config.rubans, nouvelle_config.tetes)
